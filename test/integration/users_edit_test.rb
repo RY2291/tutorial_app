cb4f3c76a4e856cc5efbc5a2703f612c1219b8ea
RY2291 <prgr2498@gmail.com>
2022-03-05 22:12:07 +0900

    [fin]10.1.4

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "unsuccessful edit" do
+    get edit_user_path(@user.id)
+    assert_template "users/edit"
+    patch user_path(@user.id), params: { user: { name:  "",
+                                            email: "foo@invalid",
+                                            password:              "foo",
+                                            password_confirmation: "bar" } }
+    assert_template 'users/edit'
+    assert_select "div.alert", "The form contains 4 errors."
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user.id)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user.id), params: { user: { name: name,
+                                                 email: email,
+                                                 password: "",
+                                                 password_confirmation: ""}}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    # データベースから最新のユーザー情報を読み込み
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
