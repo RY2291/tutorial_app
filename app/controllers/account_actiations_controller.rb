f545c98e3e19604d2664676823c02a6e00e64b1a
RY2291 <prgr2498@gmail.com>
2022-03-08 22:35:14 +0900

    [fin]11.1.1

---
+++ app/controllers/account_actiations_controller.rb
@@ -0,0 +1,3 @@
+class AccountActiationsController < ApplicationController
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  # オブジェクトが保存される前にbefore_saveが実行される
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  # オブジェクトが保存される前にbefore_saveが実行される(作成と更新の両方で呼び出される)
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -36,4 +37,15 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    def downcase_email
+      self.email.downcase!
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
